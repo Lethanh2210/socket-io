20eec7a3d679998144da6b225ec512793dfff31f
Your Name <kzxlmdtn888@gmail.com>
2022-06-21 14:26:11 +0700

    ..

---
+++ to-do-List/public/js/app.js
@@ -0,0 +1,27 @@
+const socket = io('http://localhost:3000')
+let index = 0;
+$('#todolistForm').submit(function (e) {
+    e.preventDefault();
+    const task = $('#task').val();
+    socket.emit('addTask', task);
+    insertTask(task, index);
+    index++;
+    $('#task').val('').focus();
+    return false;
+});
+
+socket.on('addTask', function(data) {
+    insertTask(data.task, data.index);
+});
+
+// socket.on('updateTask', function(todolist) {
+//     $('#todolist').empty();
+//     todolist.forEach(function(task, index) {
+//         insertTask(task, index);
+//     });
+// });
+
+function insertTask(task, index) {
+    $('#todolist').append('<li><a class="delete" href="#" data-index="' + index + '">✘</a> ' + task  + '</li>');
+}
+
---
+++ to-do-List/server.js
@@ -0,0 +1,45 @@
+const { createServer } = require("http");
+const fs = require('fs');
+const { Server } = require("socket.io");
+
+const mimeTypes = {
+    "html": "text/html",
+    "js": "text/javascript",
+    "css": "text/css"
+};
+
+const httpServer = createServer(function (req, res){
+    if (req.url === '/') {
+        res.writeHead(200, { "Content-Type": "text/html" });
+        fs.createReadStream('./templates/index.html').pipe(res)
+    }
+    /* đọc file css/js */
+    const filesDefences = req.url.match(/\.js|.css/);
+    if (filesDefences) {
+        const extension = mimeTypes[filesDefences[0].toString().split('.')[1]];
+        res.writeHead(200, { 'Content-Type': extension });
+        fs.createReadStream(__dirname + "/"+ req.url).pipe(res)
+    }
+});
+
+const io = new Server(httpServer);
+const todoList = [];
+let index = 0;
+
+/* Quản lý dữ liệu webscoket */
+io.on('connection', socket => {
+    /* Lắng nghe sự kiện addTask từ phía client */
+    socket.on('addTask', task => {
+        todoList.push(task)
+        /* Gửi sự kiện addTask cho tất cả người dùng real-time */
+        socket.broadcast.emit('addTask', {task: task, index: index})
+        index++;
+    })
+    // socket.emit('updateTask', todoList);
+})
+
+httpServer.listen(3000, 'localhost', function (){
+    console.log('Server running in http://localhost:3000')
+})
+
+
